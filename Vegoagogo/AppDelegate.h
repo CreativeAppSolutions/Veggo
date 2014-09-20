2229b546c7f94823638fdf2120d26aa937c0736b
Kevin Messina <kevinmessina@DVM-Desktop.local>
2014-09-20 08:44:04 -0400

    Initial Commit

---
+++ Vegoagogo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Vegoagogo
+//
+//  Created by Kevin Messina on 9/20/14.
+//  Copyright (c) 2014 DVMagic Studios, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Vegoagogo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Vegoagogo
+//
+//  Created by Kevin Messina on 9/20/14.
+//  Copyright (c) 2014 DVMagic Studios, Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
